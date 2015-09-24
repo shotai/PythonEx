82184cf05885dcaf5eec635bc7251997e5f08089
Mg <gamem.8818@gmail.com>
2015-09-24 11:22:06 -0400

    add No.70
    add python version for No.70

---
+++ 70_ClimbingStairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param {integer} n
+    # @return {integer}
+    def climbStairs(self, n):
+        dp = [1 for i in range(n+1)]
+        for i in range(2, n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
