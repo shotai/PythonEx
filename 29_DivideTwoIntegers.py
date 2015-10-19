1f245cec911ae9327eb73be153beb06f06f4c97f
Mg <gamem.8818@gmail.com>
2015-10-19 12:37:30 -0400

    update 29

--- 29_DivideTwoIntegers.py
+++ 29_DivideTwoIntegers.py
@@ -7,18 +7,16 @@ class Solution(object):
         """
         if abs(dividend)<abs(divisor):
             return 0
-        count = 0
         res = 0
-        sum = 0
         a = abs(dividend)
         b = abs(divisor)
         while a>=b:
-            sum = b
+            tmpsum = b
             count =1
-            while sum+sum<=a:
-                sum += sum
+            while tmpsum+tmpsum<=a:
+                tmpsum += tmpsum
                 count += count
-            a-=sum
+            a-=tmpsum
             res += count
         if (dividend <0 and divisor >0) or (dividend>0 and divisor < 0):
             res = 0-res
