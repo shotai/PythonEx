71bf1ed1e13fa82c817c296818e049b98a421876
Mg <gamem.8818@gmail.com>
2015-11-11 15:42:46 -0500

    add 241

---
+++ 241_DifferentWaystoAddParentheses.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def diffWaysToCompute(self, input):
+        """
+        :type input: str
+        :rtype: List[int]
+        """
+        if input.isdigit():
+            return [int(input)]
+        res = []
+        for i in range(len(input)):
+            if input[i] in "-+*":
+                res1 = self.diffWaysToCompute(input[:i])
+                res2 = self.diffWaysToCompute(input[i+1:])
+                res += [eval(str(k)+input[i]+str(j)) for k in res1 for j in res2]
+        return res
