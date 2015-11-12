14e1679aa73f40916c1425f6b33a7c54abe46a0c
Mg <gamem.8818@gmail.com>
2015-11-12 16:59:47 -0500

    add 1

---
+++ 1_TwoSum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] in dict:
+                return [dict[target-nums[i]]+1, i+1]
+            dict[nums[i]] = i
