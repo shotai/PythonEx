d8029f58f0e967570831ea04b7ae3950f82c4a86
Mg <gamem.8818@gmail.com>
2015-12-02 16:52:07 -0500

    add 299

---
+++ 299_BullsandCows.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    import collections
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        s, g = collections.Counter(secret), collections.Counter(guess)
+        a = sum(i == j for i, j in zip(secret, guess))
+        return '%sA%sB' % (a, sum((s & g).values()) - a)
