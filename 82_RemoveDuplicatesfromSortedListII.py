eca51efad4632c606acce1097c1905e4a8d4eec9
Mg <gamem.8818@gmail.com>
2015-12-08 13:38:50 -0500

    Create 82_RemoveDuplicatesfromSortedListII.py

---
+++ 82_RemoveDuplicatesfromSortedListII.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        dummy.next = head
+        p = dummy
+        tmp = dummy.next
+        while p.next:
+            while tmp.next and tmp.next.val == p.next.val:
+                tmp = tmp.next
+            if tmp == p.next:
+                p = p.next
+                tmp = tmp.next
+            else:
+                p.next = tmp.next
+        return dummy.next
