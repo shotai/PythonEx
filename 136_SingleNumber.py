970f2e2478966fa533b84f4ad6fb2146f33d3033
Mg <gamem.8818@gmail.com>
2015-12-28 09:37:58 -0500

    Update 136_SingleNumber.py

--- 136_SingleNumber.py
+++ 136_SingleNumber.py
@@ -8,3 +8,12 @@ class Solution(object):
         for i in nums[1:]:
             res = res^i
         return res
+        
+    def singleNumber2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        expect = sum(set(nums))
+        res = expect*2-sum(nums)
+        return res
