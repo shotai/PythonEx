9ac8e07f4733d425a94ebc061cd3a83433dec2e6
Mg <gamem.8818@gmail.com>
2015-12-28 13:55:29 -0500

    Create 137_SingleNumberII.py

---
+++ 137_SingleNumberII.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ones = twos = 0
+        for i in nums:
+            ones = (ones^i) & ~twos
+            twos = (twos^i) & ~ones
+        return ones
