985f6259af5ac5cb84dc5fc99db1ec3f53ec05b5
Mg <gamem.8818@gmail.com>
2015-12-28 09:34:00 -0500

    Create 136_SingleNumber.py

---
+++ 136_SingleNumber.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = nums[0]
+        for i in nums[1:]:
+            res = res^i
+        return res
