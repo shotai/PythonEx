43b241caf298b53045b6790b7e48349d2139620c
Mg <gamem.8818@gmail.com>
2015-12-30 12:28:20 -0500

    Update 222_CountCompleteTreeNodes.py

--- 222_CountCompleteTreeNodes.py
+++ 222_CountCompleteTreeNodes.py
@@ -23,3 +23,24 @@ class Solution(object):
             return pow(2, leftd) + self.countNodes(root.right)
         else:
             return pow(2, rightd) + self.countNodes(root.left)
+            
+    
+    def countNodes_iter(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def height(node):
+            return -1 if not node else 1+height(node.left)
+        
+        h = height(root)
+        nodes = 0
+        while root:
+            if height(root.right) == h-1:
+                nodes += 2**h
+                root = root.right
+            else:
+                nodes += 2 ** (h-1)
+                root = root.left
+            h -= 1
+        return nodes
